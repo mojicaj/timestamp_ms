299b47deb9c5256c21a483e209d04dc9aed0cfbf
Glitch (pebble-harmonica) <none>
2017-11-10 21:24:37 +0000

    Checkpoint

--- time.js
+++ time.js
@@ -1,7 +1,26 @@
 
+var natural, unix;
+var date = new Date();
 
 var time = function (str) {
-  return  { "you entered": str };
+  
+  if (/^\/api\/parsetime/.test(str)) {
+    natural = prsTime(str);
+  } else if (/^\/api\/unixtime/.test(str)) {
+    unix = unxTime(Number(str));
+  }
+  
+  return  { "unix": (unix || str), "natural": (natural || str)  };
 }
+
+function prsTime() {
+  
+}
+
+function unxTime(time) {
+  return date.getTime(time);
+}
+
+
 
 module.exports = time;
