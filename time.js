9262707d68fd9c1da425f79fd15254840e291376
Glitch (pebble-harmonica) <none>
2017-11-11 01:26:26 +0000

    Checkpoint

--- time.js
+++ time.js
@@ -5,27 +5,26 @@ var time = function (str) {
     natural = prsTime(str);    
   } else {
     unix = unxTime(str);
-    //unix = (typeof(unix) == 'Number')
     natural = prsTime(unix);
   }
   
-  console.log(isNaN(unix));
   if ( !natural && !unix ) {
     return  { "unix": 'null', "natural": 'null' };
   } else {
-    return  { "unix": (unix || str), "natural": (natural || str) };
+    return  { "unix": (unix || Number(str)), "natural": (natural || str) };
   }
 }
 
 function prsTime(tm) {
-  var date = new Date(tm*1000);  
+  var date = new Date(tm*1000);
+  console.log(date);
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   
   var year = date.getFullYear(tm);
   var month = date.getMonth(tm);
   var day = date.getDate(tm);
   
-  return months[month]+' '+day+', '+year;
+  return months[month]+' '+day+', '+year || ;
 }
 
 function unxTime(tm) {
